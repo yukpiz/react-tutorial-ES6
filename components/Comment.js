d287cfa4924324e261724588baba352cd2fcdbb9
yukpiz <yukpiz@gmail.com>
2016-12-02 18:56:10 +0900

    Render markdown texts.

--- components/Comment.js
+++ components/Comment.js
@@ -1,13 +1,15 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
+import Marked from "marked"
 
 export default class Comment extends Component {
   render() {
+    var rawMarkup = Marked(this.props.children.toString(), {sanitize: true});
     return(
       <div className="comment">
         <h3 className="commentAuthor">
           {this.props.author}
         </h3>
-        {this.props.children}
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
       </div>
     );
   }
