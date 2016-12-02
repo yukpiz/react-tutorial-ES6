51f17a2f70abbcd30d3cacbe97dfab8f4868899c
yukpiz <yukpiz@gmail.com>
2016-12-02 19:08:48 +0900

    Request json file and rendering data.

--- components/CommentBox.js
+++ components/CommentBox.js
@@ -1,17 +1,33 @@
 import React, { Component } from "react"
 import CommentList from "./CommentList"
 import CommentForm from "./CommentForm"
+import $ from "jquery"
 
 export default class CommentBox extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    $.ajax({
+      url: this.props.url,
+      dataType: "json",
+      cache: false,
+      success: (data) => { this.setState({data: data}); },
+      error: (xhr, status, err) => {
+        console.error(this.props.url, status, err.toString());
+      }
+    });
   }
 
   render() {
     return(
       <div className="commentBox">
         <h2>Comments</h2>
-        <CommentList data={this.props.data}/>
+        <CommentList data={this.state.data}/>
         <CommentForm/>
       </div>
     );
--- index.js
+++ index.js
@@ -3,13 +3,7 @@ import { render } from "react-dom";
 
 import CommentBox from "./components/CommentBox"
 
-var data = [
-  {author: "Pete Hunt", text: "This is one comment"},
-  {author: "Jordan Walke", text: "This is *another* comment"},
-  {author: "Yukpiz", text: "foo!!"},
-];
-
 render(
-  <CommentBox data={data}/>,
+  <CommentBox url="comments.json"/>,
   document.getElementById("container")
 );
